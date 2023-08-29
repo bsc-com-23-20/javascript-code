acbf97bfa1352030eee32bc7f5935ec3eb56ea37
bsc-com-23-20 <bsc-com-23-20@unima.ac.mw>
2023-08-30 01:13:16 +0200

    random number generator

--- index.js
+++ index.js
@@ -13,12 +13,43 @@ let username = window.prompt("whats your name?");
 console.log(username);
 
 //using 
-document.getElementById("mybutton").onclick = function(){
-    myName = document.getElementById("myText").value;
-    console.log(myName)
-}
+// document.getElementById("mybutton").onclick = function(){
+//     myName = document.getElementById("myText").value;
+//     console.log(myName)
+// }
 
 //type conversion
 
 // every input is automatically a string, you have to convert to a number or any other variable type?
 // math.functions
+
+//our first counter program
+let count = 0;
+document.getElementById("decreasebtn").onclick = function(){
+    count -= 1;
+    document.getElementById("counter").innerHTML = count;
+    console.log(count)
+}
+document.getElementById("resetbtn").onclick = function(){
+    count = 0;
+    document.getElementById("counter").innerHTML = count;
+    console.log(count)
+}
+document.getElementById("increasebtn").onclick = function(){
+    count += 1;
+    document.getElementById("counter").innerHTML = count;
+    console.log(count)
+}
+
+// random number generator
+let x,y;
+
+document.getElementById("rollbtn").onclick = function(){
+    x = Math.floor(Math.random()* 6)
+    document.getElementById("xlabel").innerHTML = x;
+    console.log(x)
+}
+document.getElementById("rollbtn").onclick = function(){
+    y = Math.floor(Math.random()* 6)
+    document.getElementById("ylabel").innerHTML = x;
+    console.log(y)}

a6a2832a2eabbc2198491cbf4732aa6286c8e315
bsc-com-23-20 <bsc-com-23-20@unima.ac.mw>
2023-08-29 14:03:27 +0200

    what i have done so far

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("i like pizza!");
+
+window.alert("i really love it")
+let firstName = "bro"
+
+
+let students = 21/4;
+console.log(students)
+document.getElementById("p1").innerHTML ="hello" + firstName;
+
+//accepting user input, window prompt
+let username = window.prompt("whats your name?");
+console.log(username);
+
+//using 
+document.getElementById("mybutton").onclick = function(){
+    myName = document.getElementById("myText").value;
+    console.log(myName)
+}
+
+//type conversion
+
+// every input is automatically a string, you have to convert to a number or any other variable type?
+// math.functions
